ef4ea03530ff115488e889aaf528c2172adff373
ElenaG <146771196+ElenaGuillen@users.noreply.github.com>
2023-11-19 09:31:59 +0100

    Update Pair Programing Subconsultas.sql

--- Pair Programing Subconsultas.sql
+++ Pair Programing Subconsultas.sql
@@ -1,66 +1,79 @@
-use northwind;
-Ejercicio 1. Subconsultas.
-# Falta terminar
-SELECT order_id, customer_id, employee_id, order_date, required_date
-FROM orders WHERE employee_id IN (
-SELECT employee_id
-FROM orders
-Where MAX(order_date);
+Use northwind;
 
-SELECT order_id, customer_id, employee_id, order_date, required_date -- Deuvelve los empleados q han hecho un pedido con fecha máxima
-FROM orders where order_date = (Select MAX(order_date)
-from orders);
-
+-- 1. Extraer los pedidos con el máximo "order_date" para cada empleado
 SELECT order_id, customer_id, employee_id, order_date, required_date
 FROM orders AS a where order_date = (Select MAX(order_date)
 from orders AS b where a.employee_id = b.employee_id);
 
-SELECT order_id, customer_id, employee_id, order_date, required_date, 
-FROM orders WHERE employee_id IN (
-SELECT employee_id, customer_id, MAX(order_date) AS max_order_date 
-FROM orders
-GROUP BY employee_id, customer_id;
+Select MAX(order_date) -- Esta sería la subconsulta 
+from orders
+group by employee_id;
 
-SELECT order_id, customer_id, employee_id
-FROM orders WHERE employee_id IN (
-SELECT employee_id, sus MAX(order_date) AS max_order_date 
-FROM orders
-GROUP BY employee_id);
+-- 2. Extraer  el precio unitario máximo (unit_price) de cada producto vendido
+select product_id, unit_price
+from products;
 
+-- con subquery correlacionada
+select product_id, unit_price 
+from products as p
+where unit_price = (Select Max(unit_price) 
+from products as p1 where p.product_id = p1.product_id);
 
+-- 3. Extraer información de los productos "Beverages"
+select product_id, product_name, category_id
+from products
+where category_id = 1;
 
-
-Ejercicio 2
-Pendiente
-
-
-
-Ejercicio 3
+-- con subquery 
 SELECT product_id,product_name, category_id
 FROM products WHERE category_id = (
         SELECT category_id
         FROM categories
-        WHERE category_name = 'Beverages')
+        WHERE category_name = 'Beverages');
         
-Ejercicio 4
+-- con subquery correlacionada
+select product_id, product_name, category_id
+from products as pr
+where category_id = (select category_id 
+		        from products as pr2 
+			where category_id = 1 and pr.product_id = pr2.product_id);
+        
+-- 4. Extraer la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país
 SELECT DISTINCT country
 FROM customers
 WHERE country NOT IN (SELECT DISTINCT country
         FROM suppliers
         WHERE country IS NOT NULL);
-Ejercicio 5
-Pendiente
 
-Ejercicio 6
-SELECT
-    product_name,
-    unit_price
-FROM
-    products
-ORDER BY
-    unit_price DESC;
-WHERE unit_price >= 10 (
-        SELECT COUNT(DISTINCT unit_price)
-        FROM products
-        )
+-- 5. Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread" (producto id 6) en un sólo pedido
+select *
+from customers as c
+where customer_id in (select customer_id
+from orders as o
+where order_id in (select order_id
+from order_details as d
+where product_id = 6 AND quantity > 20));
 
+-- 6. Extraer los 10 productos más caros
+SELECT product_name, unit_price
+from products
+order by unit_price DESC
+limit 10;
+
+-- con subquery correlacionada
+SELECT product_name, unit_price
+from products as pr
+where product_name = (SELECT product_name
+from products as pr1 where pr.product_name = pr1.product_name)
+order by unit_price DESC
+limit 10;
+
+-- 7. Extraer el producto más popular y la cantidad que se compró
+select product_name -- faltaria mostrar la suma de las cantidades
+from products
+where product_id in (select product_id
+from order_details as j
+group by product_id
+having SUM(quantity) >= ALL (select SUM(quantity)
+				from order_details as h
+				group by product_id));
